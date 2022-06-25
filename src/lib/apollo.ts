4ac4e04ad52878a5f8391b9196ecc4256c3cf669
richard <richard_bueno@ymail.com>
2022-06-25 16:30:44 -0300

    correcao env

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -3,6 +3,6 @@ export const client = new ApolloClient({
   uri: import.meta.env.VITE_API_URL,
   cache: new InMemoryCache(),
   headers: {
-   'Authorization': `Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN}`,
+   'Authorization': `Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN}`
   },
 });
